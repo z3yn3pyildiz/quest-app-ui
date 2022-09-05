ce26a127088b9a89f4f0822127d469baefed8ef9
zeynep yıldız <zeynepyildiz113@gmail.com>
2022-09-05 11:42:56 +0300

    feat:created base components

--- src/components/post/Post.js
+++ src/components/post/Post.js
@@ -1,43 +1,17 @@
-import React,{useState,useEffect} from "react"
+
 //import ReactDOM from "react-dom"
+import React from "react";
+import './Post.scss'
 
-function Post(){
-    const [error,setError]=useState(null)
-    const [isLoaded, setIsLoaded] = useState (false)
-    const [postList, setPostList] = useState ([])
 
-    useEffect(()=>{
-        fetch("/posts")
-        .then(res=>res.json())
-        .then(
-            (result) => {
-                setIsLoaded(true)
-                setPostList(result)
-            },
-            (error) =>{
-                setIsLoaded(true)
-                setError(error)
-            }
-        )
-    },[])
+function Post(props){
+  const {title,text}=props;
 
-    if(error){
-        return <div> Error !</div>
-    }
-    else if(!isLoaded){
-        return <div> Loading ...</div>
-    }
-    else{
-        return(
-            <ul>
-                {postList.map(post =>(
-                    <li>
-                        {post.title} - {post.text}
-                    </li>
-                ))}
-            </ul>
-        );
-    }
+  return(
+    <div className="postContainer">
+        {title}-{text}
+    </div>
+  )
 }
 
 export default Post;
